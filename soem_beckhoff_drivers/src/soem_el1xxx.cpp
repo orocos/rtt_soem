b109a9f1f8afbaf285cff72320fcce8b6458117c
Ruben Smits <ruben.smits@mech.kuleuven.be>
2010-11-16 08:32:41 +0000

    fix licenses

--- soem_beckhoff_drivers/src/soem_el1xxx.cpp
+++ soem_beckhoff_drivers/src/soem_el1xxx.cpp
@@ -1,9 +1,32 @@
-/*!
-* @author: Ruben Smits and Koen Buys
-* @description: Driver for Beckhoff EL1124 module, 4x5V digital inputs
-* @version: 9 April 2010 - 1.0
-* @license: LGPL (see Berlios - SOEM)
-*/
+/***************************************************************************
+  tag: Ruben Smits  Tue Nov 16 09:31:20 CET 2010  soem_el1xxx.cpp
+
+                        soem_el1xxx.cpp -  description
+                           -------------------
+    begin                : Tue November 16 2010
+    copyright            : (C) 2010 Ruben Smits, Koen Buys
+    email                : first.last@mech.kuleuven.be
+
+ ***************************************************************************
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Lesser General Public            *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2.1 of the License, or (at your option) any later version.    *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
+ *   Lesser General Public License for more details.                       *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General Public      *
+ *   License along with this library; if not, write to the Free Software   *
+ *   Foundation, Inc., 59 Temple Place,                                    *
+ *   Suite 330, Boston, MA  02111-1307  USA                                *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
 #include "soem_el1xxx.h"
 #include <soem_master/soem_driver_factory.h>
 
